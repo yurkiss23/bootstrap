15db1be60b3e5dd19bb4162b1c6a2d19d6e33256
yurkiss <terranet23@gmail.com>
2019-07-16 11:52:46 +0300

    edit array's dialogs home

--- js/array.js
+++ js/array.js
@@ -12,6 +12,8 @@ window.addEventListener('load',function(){
 
     var listBtnUpdate=document.getElementsByClassName("updateRow");
     var listBtnDelete=document.getElementsByClassName("deleteRow");
+    let listBtnSuccess=document.querySelectorAll("[data-success]");
+    let listBtnClose=document.querySelectorAll("[data-close]");
     //var listCheckBox=document.getElementsByClassName("checkRow");
 
     //let Show=document.getElementById("btnShow");
@@ -57,30 +59,9 @@ window.addEventListener('load',function(){
     //add_row.onclick=addRow;
     
     function updateRow(){
-        let qwe=updRow.getElementById("inputName");
-        // alert(qwe);
-        updRow.classList.remove("hide");
-
-        let listBtnSuccess=document.querySelectorAll("[data-success]");
-        let listBtnClose=document.querySelectorAll("[data-close]");
-
-        for(let i=0;i<listBtnSuccess.length;i++){
-            listBtnSuccess[i].onclick=function upd(){
-                updRow.classList.add("hide");
-                dlgPrgs.classList.remove("hide");
-                setTimeout(function(){
-                    dlgPrgs.classList.add("hide");
-                    notify.classList.remove("hide");
-                },3000);
-                setTimeout(function(){notify.classList.add("hide")},4000);
-            }
-        }
-        for(let i=0;i<listBtnClose.length;i++){
-            listBtnClose[i].onclick=function(){
-                updRow.classList.add("hide");
-            }
-        }
-
+        let newName=document.getElementById("inputName");
+        let newPhn=document.getElementById("inputPhn");
+        
         let parent=this.parentNode;
         while(parent.nodeName!=="TR"){
             parent=parent.parentNode;
@@ -90,44 +71,69 @@ window.addEventListener('load',function(){
         // parent.children[2].innerHTML=prompt("new name",parent.children[2].innerHTML);
         // parent.children[3].innerHTML=prompt("new phone",parent.children[3].innerHTML);
 
-        
+        newName.value=parent.children[2].innerHTML;
+        newPhn.value=parent.children[3].innerHTML;
+
+        updRow.classList.remove("hide");
+
+        for(let i=0;i<listBtnSuccess.length;i++){
+            listBtnSuccess[i].onclick=function(){
+                let flag=true;
+                if(newName.value==parent.children[2].innerHTML&&newPhn.value==parent.children[3].innerHTML){
+                    flag=false;
+                }
+                parent.children[2].innerHTML=newName.value;
+                parent.children[3].innerHTML=newPhn.value;
+                updRow.classList.add("hide");
+                dlgPrgs.classList.remove("hide");
+                setTimeout(function(){
+                    dlgPrgs.classList.add("hide");
+                    if(!flag){
+                        notifyWrg.classList.remove("hide");
+                    }else{
+                        notify.classList.remove("hide");
+                    }
+                },3000);
+                if(!flag){
+                    setTimeout(function(){notify.classList.add("hide")},4500);
+                }else{
+                    setTimeout(function(){notify.classList.add("hide")},4500);
+                }
+            }
+        }
+        for(let i=0;i<listBtnClose.length;i++){
+            listBtnClose[i].onclick=function(){
+                updRow.classList.add("hide");
+            }
+        }
     }
     function deleteRow(){
         let parent=this.parentNode;
         while(parent.nodeName!=="TR"){
             parent=parent.parentNode;
         }
-        parent.remove();
-    }
-    // function addRow(){
-    //     var addNewRow=document.createElement("tr");
-    //     addNewRow.innerHTML=
-    //     `<td>
-    //     <input class="form-control checkRow" type="checkbox">
-    //     </td>
-    //     <td>
-    //     <img class="thumbnail"
-    //     src="https://cdn.arstechnica.net/wp-content/uploads/2016/02/5718897981_10faa45ac3_b-640x624.jpg" alt="">
-    //     </td>
-    //     <td>qwe</td>
-    //     <td>123</td>
-    //     <td>
-    //     <button class="btn btn-warning updateRow">Update</button>
-    //     <button class="btn btn-danger deleteRow">Delete</button>
-    //     </td>`;
-    //     // photo.value=null;
-    //     // addName.value=null;
-    //     // phone.value=null;
-    //     data.appendChild(addNewRow);
-    //     for(let i=0;i<listBtnUpdate.length;i++){
-    //         listBtnUpdate[i].onclick=updateRow;
-    //     }
-    //     for(let i=0;i<listBtnDelete.length;i++){
-    //         listBtnDelete[i].onclick=deleteRow;
-    //     }
-    //     alert("add row");
-    // }
 
+        delRow.classList.remove("hide");
+
+        for(let i=0;i<listBtnSuccess.length;i++){
+            listBtnSuccess[i].onclick=function(){
+                delRow.classList.add("hide");
+                dlgPrgs.classList.remove("hide");
+                setTimeout(function(){
+                    dlgPrgs.classList.add("hide");
+                    parent.remove();
+                    notify.classList.remove("hide");
+                },1000);
+                setTimeout(function(){notify.classList.add("hide")},2500);
+            }
+        }
+
+        for(let i=0;i<listBtnClose.length;i++){
+            listBtnClose[i].onclick=function(){
+                delRow.classList.add("hide");
+            }
+        }
+    }
     btnShow.onclick=function(){
         dlgPrgs.classList.remove("hide");
         setTimeout(function(){
