1891e2e5708b4a732c58f0ba1723938039f8b650
yurkiss <terranet23@gmail.com>
2019-07-23 17:50:42 +0200

    edit gallery work

--- js/gallery.js
+++ js/gallery.js
@@ -1,10 +1,12 @@
 $(function(){
-    // const addImg=$("#addImg");
-    
+    const addImg=$("#addImg");
+    const dlgCrop=$("#dlgCrop");
     let galleryArr=null;
     
-    $("#addImg").on("click",function(){
+    addImg.on("click",function(){
         $("#fileSelect").click();
     });
-    
+    $("[data-close]").on("click",function(){
+        dlgCrop.hide();
+    });
 })
